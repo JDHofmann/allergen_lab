e1fadf1e47b50ed3d7dc12b6b0af92a5b928033b
jdhofmann <justindhofmann@gmail.com>
2020-09-19 16:15:39 -0400

    not dead

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+RecipeIngredient.destroy_all
+UserIngredient.destroy_all
+User.destroy_all
+Ingredient.destroy_all
+Recipe.destroy_all
+
+jake = User.create(name: "Jake")
+tammy = User.create(name: "Tammy")
+
+steak = Recipe.create(name: "Steak", user_id: jake.id)
+salad = Recipe.create(name: "salad", user_id: tammy.id)
+### 
+pb_soup = Recipe.create(name: "Peanut Butter Soup", user: jake)
+
+pb = Ingredient.create(name: "peanut butter")
+water = Ingredient.create(name: "water")
+
+RecipeIngredient.create(recipe_id: steak.id, ingredient_id: water.id )
+RecipeIngredient.create(recipe: pb_soup, ingredient: pb )
+
+UserIngredient.create(user: tammy, ingredient: pb)
